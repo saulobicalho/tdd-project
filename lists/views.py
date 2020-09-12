99bc46e5296c945fb6fd9a89ac199a28282efd31
sauloweikert <sbicalho86@gmail.com>
2020-09-12 19:59:33 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 1

--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import redirect, render
 from lists.models import Item
 
-
 def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
