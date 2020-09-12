fed1d1283de31c70516c3fddba61e73ad51f2cc6
sauloweikert <sbicalho86@gmail.com>
2020-09-12 19:07:52 -0300

    Apresentar Múltiplos Itens na Listagem

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import redirect, render
 from lists.models import Item
 
+
 def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
         return redirect('/')
 
-    return render(request, 'home.html')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
